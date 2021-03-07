01de5abd33124af2111e3b89d99c0d66cbc9b300
MaiKuraki <mai.k@live.com>
2021-03-08 00:34:56 +0800

    Camera Movement

--- SURV_UE4/Source/SURV/Private/Pawns/SURVCameraComponent.cpp
+++ SURV_UE4/Source/SURV/Private/Pawns/SURVCameraComponent.cpp
@@ -2,4 +2,191 @@
 
 
 #include "Pawns/SURVCameraComponent.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/LocalPlayer.h"
+#include "SURVGameState.h"
+#include "Engine/World.h"
+#include "GameFramework/SpectatorPawn.h"
+#include "Pawns/SURVSpectatorPawnMovement.h"
 
+USURVCameraComponent::USURVCameraComponent(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	ZoomAlpha = 1.0f;
+	CameraScrollSpeed = 4000.0f;
+	MinZoomLevel = 0.4f;
+	MaxZoomLevel = 1.0f;
+}
+
+void USURVCameraComponent::GetCameraView(float DeltaTime, FMinimalViewInfo& OutResult)
+{
+	APlayerController* Controller = GetPlayerController();
+	if (Controller)
+	{
+		OutResult.FOV = 30.f;
+		const float CurrentOffset = MinCameraOffset + ZoomAlpha * (MaxCameraOffset - MinCameraOffset);
+		FVector Pos2 = Controller->GetFocalLocation();
+		OutResult.Location = Controller->GetFocalLocation() - FixedCameraAngle.Vector() * CurrentOffset;
+		OutResult.Rotation = FixedCameraAngle;
+	}
+}
+
+void USURVCameraComponent::OnZoomIn()
+{
+	SetZoomLevel(ZoomAlpha - 0.1f);
+}
+
+void USURVCameraComponent::OnZoomOut()
+{
+	SetZoomLevel(ZoomAlpha + 0.1f);
+}
+
+void USURVCameraComponent::UpdateCameraMovement(const APlayerController* InPlayerController)
+{
+	//	No mouse support on mobile
+#if PLATFORM_DESKTOP
+	ULocalPlayer* const LocalPlayer = Cast<ULocalPlayer>(InPlayerController->Player);
+	if (LocalPlayer && LocalPlayer->ViewportClient && LocalPlayer->ViewportClient->Viewport)
+	{
+		FVector2D MousePosition;
+		if (LocalPlayer->ViewportClient->GetMousePosition(MousePosition) == false)
+		{
+			return;
+		}
+
+		FViewport* Viewport = LocalPlayer->ViewportClient->Viewport;
+		const float ScrollSpeed = 60.0f;
+		const FIntPoint ViewportSize = Viewport->GetSizeXY();
+		const uint32 ViewLeft = FMath::TruncToInt(LocalPlayer->Origin.X * ViewportSize.X);
+		const uint32 ViewRight = ViewLeft + FMath::TruncToInt(LocalPlayer->Size.X * ViewportSize.X);
+		const uint32 ViewTop = FMath::TruncToInt(LocalPlayer->Origin.Y * ViewportSize.Y);
+		const uint32 ViewBottom = ViewTop + FMath::TruncToInt(LocalPlayer->Size.Y * ViewportSize.Y);
+
+		const float MaxSpeed = CameraScrollSpeed * FMath::Clamp(ZoomAlpha, 0.3f, 1.0f);
+
+		ASURVGameState const* const MyGameState = GetWorld()->GetGameState<ASURVGameState>();
+		bool bNoScrollZone = false;
+		FVector MouseCoords(MousePosition, 0.0f);
+		for (int iZone = 0; iZone < NoScrollZones.Num(); iZone++)
+		{
+			FBox EachZone = NoScrollZones[iZone];
+			if (EachZone.IsInsideXY(MouseCoords) == true)
+			{
+				bNoScrollZone = true;
+			}
+		}
+
+		const uint32 MouseX = MousePosition.X;
+		const uint32 MouseY = MousePosition.Y;
+		float SpectatorCameraSpeed = MaxSpeed;
+		ASpectatorPawn* SpectatorPawn = NULL;
+		if (GetPlayerController() != NULL)
+		{
+			SpectatorPawn = GetPlayerController()->GetSpectatorPawn();
+			if (SpectatorPawn->GetMovementComponent() != NULL)
+			{
+				SpectatorCameraSpeed = GetDefault<USURVSpectatorPawnMovement>(SpectatorPawn->GetMovementComponent()->GetClass())->MaxSpeed;
+			}
+		}
+		if (!bNoScrollZone)
+		{
+			if (MouseX >= ViewLeft && MouseX <= (ViewLeft + CameraActiveBorder))
+			{
+				const float delta = 1.0f - float(MouseX - ViewLeft) / CameraActiveBorder;
+				SpectatorCameraSpeed = delta * MaxSpeed;
+				MoveRight(-ScrollSpeed * delta);
+			}
+			else if (MouseX >= (ViewRight - CameraActiveBorder) && MouseX <= ViewRight)
+			{
+				const float delta = float(MouseX - ViewRight + CameraActiveBorder) / CameraActiveBorder;
+				SpectatorCameraSpeed = delta * MaxSpeed;
+				MoveRight(ScrollSpeed * delta);
+			}
+
+			if (MouseY >= ViewTop && MouseY <= (ViewTop + CameraActiveBorder))
+			{
+				const float delta = 1.0f - float(MouseY - ViewTop) / CameraActiveBorder;
+				SpectatorCameraSpeed = delta * MaxSpeed;
+				MoveForward(ScrollSpeed * delta);
+			}
+			else if (MouseY >= (ViewBottom - CameraActiveBorder) && MouseY <= ViewBottom)
+			{
+				const float delta = (MouseY - (ViewBottom - CameraActiveBorder)) / CameraActiveBorder;
+				SpectatorCameraSpeed = delta * MaxSpeed;
+				MoveForward(-ScrollSpeed * delta);
+			}
+
+			if (SpectatorPawn != NULL)
+			{
+				UFloatingPawnMovement* PawnMovementComponent = Cast<UFloatingPawnMovement>(SpectatorPawn->GetMovementComponent());
+				if (PawnMovementComponent)
+				{
+					PawnMovementComponent->MaxSpeed = SpectatorCameraSpeed;
+				}
+			}
+		}
+	}
+#endif
+	NoScrollZones.Empty();
+}
+
+void USURVCameraComponent::MoveForward(float Val)
+{
+	APawn* OwnerPawn = GetOwnerPawn();
+	if (OwnerPawn != NULL)
+	{
+		APlayerController* Controller = GetPlayerController();
+		if ( Val != 0.f && Controller != NULL)
+		{
+			const FRotationMatrix R(Controller->PlayerCameraManager->GetCameraRotation());
+			const FVector WorldSpaceAccel = R.GetScaledAxis(EAxis::X) * 100.f;
+
+			//	transform to world space and add it
+			OwnerPawn->AddMovementInput(WorldSpaceAccel, Val);
+		}
+	}
+}
+
+void USURVCameraComponent::MoveRight(float Val)
+{
+	APawn* OwnerPawn = GetOwnerPawn();
+	if (OwnerPawn != NULL)
+	{
+		APlayerController* Controller = GetPlayerController();
+		if (Val != 0.f && Controller != NULL)
+		{
+			const FRotationMatrix R(Controller->PlayerCameraManager->GetCameraRotation());
+			const FVector WorldSpaceAccel = R.GetScaledAxis(EAxis::Y) * 100.f;
+
+			//	transform to world space and add it
+			OwnerPawn->AddMovementInput(WorldSpaceAccel, Val);
+		}
+	}
+}
+
+void USURVCameraComponent::AddNoScrollZone(FBox InCoords)
+{
+	NoScrollZones.AddUnique(InCoords);
+}
+
+void USURVCameraComponent::SetZoomLevel(float NewLevel)
+{
+	ZoomAlpha = FMath::Clamp(NewLevel, MinZoomLevel, MaxZoomLevel);
+}
+
+APawn* USURVCameraComponent::GetOwnerPawn()
+{
+	return Cast<APawn>(GetOwner());
+}
+
+APlayerController* USURVCameraComponent::GetPlayerController()
+{
+	APlayerController* Controller = NULL;
+	APawn* Owner = GetOwnerPawn();
+	if (Owner != NULL)
+	{
+		Controller = Cast<APlayerController>(Owner->GetController());
+	}
+	return Controller;
+}
--- SURV_UE4/Source/SURV/Public/Pawns/SURVCameraComponent.h
+++ SURV_UE4/Source/SURV/Public/Pawns/SURVCameraComponent.h
@@ -7,11 +7,94 @@
 #include "SURVCameraComponent.generated.h"
 
 /**
- * 
+ *
  */
-UCLASS()
+UCLASS(config = Game, BlueprintType, HideCategories = Trigger, meta = (BlueprintSpawnableComponent))
 class SURV_API USURVCameraComponent : public UCameraComponent
 {
-	GENERATED_BODY()
-	
+	GENERATED_UCLASS_BODY()
+
+public:
+	// Begin UCameraComponent interface
+	void GetCameraView(float DeltaTime, FMinimalViewInfo& DesiredView) override;
+	// End UCameraComponent interface
+
+	/** Handle zooming in */
+	void OnZoomIn();
+
+	/** Handle zooming out */
+	void OnZoomOut();
+
+	/*
+	 *	Update the mouse controlled camera movement
+	 *
+	 *	@param	InPlayerController	The relevant player controller
+	 */
+	void UpdateCameraMovement(const APlayerController* InPlayerController);
+
+	/*
+	 *	Move the camera on the forward axis
+	 *
+	 *	@param	Val		Amount to move
+	 */
+	void MoveForward(float Val);
+
+	/*
+	 *	Move the camera on the left/right axis
+	 *
+	 *	@param	Val		Amount to move
+	 */
+	void MoveRight(float Val);
+
+	/*
+	 *	Exclude an area from the mouse scroll movement update. (This will be reset at the end of each update) 
+	 *
+	 *	@param	InCoords
+	 */
+	 void AddNoScrollZone(FBox InCoords);
+
+
+	/** Minimum amount of camera zoom (How close we can get the map). */
+	UPROPERTY(config)
+	float MinZoomLevel;
+
+	/** Maxmum amount of camera zoom (How far we can get to the map) */
+	UPROPERTY(config)
+	float MaxZoomLevel;
+
+	/** The minimum offset of the camera */
+	UPROPERTY(config)
+	float MinCameraOffset;
+
+	/** The maxmum offset of the camera */
+	UPROPERTY(config)
+	float MaxCameraOffset;
+
+	/** The angle to look down on the map */
+	UPROPERTY(config)
+	FRotator FixedCameraAngle;
+
+	/** How fast the camera moves around when the mouse is at the edge of the screen. */
+	UPROPERTY(config)
+	float CameraScrollSpeed;
+
+	/** Size of the area at the edge of the screen that will trigger camera scrolling. */
+	UPROPERTY(config)
+	uint32 CameraActiveBorder;
+
+	/** Sets the desired zoom level; clamping if necessary */
+	void SetZoomLevel(float NewLevel);
+
+private:
+	/** Return the pawn that owns this component */
+	APawn* GetOwnerPawn();
+
+	/** Return the player controller of the pawn that owns this component */
+	APlayerController* GetPlayerController();
+
+	/** Current amount of camera zoom. */
+	float ZoomAlpha;
+
+	/** List of zones to exclude from scrolling during the camera movementt update */
+	TArray<FBox> NoScrollZones;
 };
